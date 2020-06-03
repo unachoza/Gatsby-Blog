fac5bc7f1c0a930e96df0f174fbc24e3949d79ac
arianna choza <unachoza@gmail.com>
2020-06-02 20:38:46 -0400

    added pages and font-family

---
+++ blog/src/pages/my-page.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+import "./pages.css"
+
+const MyPage = () => (
+  <Layout>
+    <p>{"moving on to other pages ... ".toLocaleUpperCase()}</p>
+  </Layout>
+)
+
+export default MyPage
